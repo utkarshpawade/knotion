527d42be2b7b5b013cbdc0ee4bb441079dee834a
Utkarsh <utkarshpawadecse@gmail.com>
2025-10-22 15:21:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+app.post("/user/signup", function(req, res){
+
+})
+app.post("/user/signin", function(req, res){
+    
+})
+app.get("/courses", function(req, res){
+    
+})
+app.get("/user/purchases", function(req, res){
+    
+})
+app.post("/course/purchase", function(req, res){
+    
+})
+
+
+
+
+
+
+
+
+app.listen(3000);
