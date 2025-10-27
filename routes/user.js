64fe8bbc0dfb72c75f1d27b1b7b42c15e24a001c
Utkarsh <utkarshpawadecse@gmail.com>
2025-10-28 00:28:38 +0530

    added user signup

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,32 @@
 const {Router}=require("express");
 const userrouter=Router();
+const {usermodel}=require("../db");
+const {z}=require("zod");
 
-function usermiddleware(req, res, next){
-    const email=req.body.email;
-    const password
-}
-
-    
+const signupschema=z.object({
+    email: z.email()({message:"invalid email id"}),
+    password: z.string().min(6, {message:"password min should be 6 chars"}),
+    firstname: z.string().min(1, {message:"first name is req"}),
+    lastname.string().min(1, {message:"lastname is req"})
+});
 
 
 
 userrouter.post("/signup", function(req, res){
-
+const {email, password, firstname, lastname}=req.body;
+const existinguser=await usermodel.findOne({email});
+if(existinguser){
+    res.json({
+        message:"user already exists"
+    });
+}
+const hashedpass=await bcrypt.hash(password, 10);
+const user=await usermodel.create({
+    email:email,
+    password: hashedpass,
+    firstname,
+    lastname
+});
 })
 userrouter.post("/signin", function(req, res){
     
